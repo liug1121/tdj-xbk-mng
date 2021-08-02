015f8503fb5c3c10c763ead9d9f38dd9ddc27174
liug1121 <61231796@qq.com>
2021-08-02 17:59:03 +0800

    commit

--- src/components/main/aside.vue
+++ src/components/main/aside.vue
@@ -5,8 +5,13 @@
       <b class="title">卡管理后台</b>
     </div>
     <el-menu :default-active="path" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" @select="handlselect" background-color="#001529" active-background-color="#1890ff" router active-text-color="#fff"
-      :collapse="isCollapse" :unique-opened="true">
-        <template v-for="(item, index) in nav_menu_data">
+      :collapse="isCollapse" :unique-opened="true" :default-openeds="openedMenus">
+      <el-submenu index="root-xbk">
+        <template slot="title">
+          <i class="el-icon-s-home"></i>
+          <span>学霸卡</span>
+        </template>
+        <template v-for="(item, index) in xuebakaMenus">
           <el-submenu v-if="item.children" :index="item.path" :key="index">
             <template slot="title">
               <i :class="item.icon"></i>
@@ -22,6 +27,96 @@
             <span v-show="!isCollapse">{{item.title}}</span>
           </el-menu-item>
         </template>
+      </el-submenu>
+
+      <el-submenu index="root-device">
+        <template slot="title">
+          <i class="el-icon-s-home"></i>
+          <span>设备管理</span>
+        </template>
+        <template v-for="(item, index) in deviceMenus">
+          <el-submenu v-if="item.children" :index="item.path" :key="index">
+            <template slot="title">
+              <i :class="item.icon"></i>
+              <span>{{item.title}}</span>
+            </template>
+            <el-menu-item v-for="(subitem,subindex) in item.children" :key="subindex" :index="subitem.path">
+              <i :class="subitem.icon"></i>
+              {{subitem.title}}
+            </el-menu-item>
+          </el-submenu>
+          <el-menu-item v-else :index="item.path" :key="index">
+            <i :class="item.icon"></i>
+            <span v-show="!isCollapse">{{item.title}}</span>
+          </el-menu-item>
+        </template>
+      </el-submenu>
+
+      <el-submenu index="root-groupControl">
+        <template slot="title">
+          <i class="el-icon-s-home"></i>
+          <span>群控管理</span>
+        </template>
+        <template v-for="(item, index) in groupControlMenus">
+          <el-submenu v-if="item.children" :index="item.path" :key="index">
+            <template slot="title">
+              <i :class="item.icon"></i>
+              <span>{{item.title}}</span>
+            </template>
+            <el-menu-item v-for="(subitem,subindex) in item.children" :key="subindex" :index="subitem.path">
+              <i :class="subitem.icon"></i>
+              {{subitem.title}}
+            </el-menu-item>
+          </el-submenu>
+          <el-menu-item v-else :index="item.path" :key="index">
+            <i :class="item.icon"></i>
+            <span v-show="!isCollapse">{{item.title}}</span>
+          </el-menu-item>
+        </template>
+      </el-submenu>
+
+      <el-submenu index="root-bigflow">
+        <template slot="title">
+          <i class="el-icon-s-home"></i>
+          <span>大流量卡</span>
+        </template>
+        <template v-for="(item, index) in bigflowMenus">
+          <el-submenu v-if="item.children" :index="item.path" :key="index">
+            <template slot="title">
+              <i :class="item.icon"></i>
+              <span>{{item.title}}</span>
+            </template>
+            <el-menu-item v-for="(subitem,subindex) in item.children" :key="subindex" :index="subitem.path">
+              <i :class="subitem.icon"></i>
+              {{subitem.title}}
+            </el-menu-item>
+          </el-submenu>
+          <el-menu-item v-else :index="item.path" :key="index">
+            <i :class="item.icon"></i>
+            <span v-show="!isCollapse">{{item.title}}</span>
+          </el-menu-item>
+        </template>
+      </el-submenu>
+
+      
+
+
+        <!-- <template v-for="(item, index) in xuebakaMenus">
+          <el-submenu v-if="item.children" :index="item.path" :key="index">
+            <template slot="title">
+              <i :class="item.icon"></i>
+              <span>{{item.title}}</span>
+            </template>
+            <el-menu-item v-for="(subitem,subindex) in item.children" :key="subindex" :index="subitem.path">
+              <i :class="subitem.icon"></i>
+              {{subitem.title}}
+            </el-menu-item>
+          </el-submenu>
+          <el-menu-item v-else :index="item.path" :key="index">
+            <i :class="item.icon"></i>
+            <span v-show="!isCollapse">{{item.title}}</span>
+          </el-menu-item>
+        </template> -->
       </el-menu>
     <!-- <div v-if="usingInXuebaka == 1 && usingInDevice == 1">
       <el-menu :default-active="path" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" @select="handlselect" background-color="#001529" active-background-color="#1890ff" router active-text-color="#fff"
@@ -78,10 +173,15 @@ export default {
       path: "",
       // nav_menu_data: AsideData.nav_menu_data,
       nav_menu_data:[],
+      xuebakaMenus:[],
+      deviceMenus:[],
+      groupControlMenus:[],
+      bigflowMenus:[],
       userType: null,
       menuShow: false,
       usingInDevice: null,
-      usingInXuebaka:null
+      usingInXuebaka:null,
+      openedMenus:[]
     };
   },
   computed: {
@@ -100,6 +200,32 @@ export default {
     this.usingInDevice = window.sessionStorage.getItem('usingInDevice')
     this.usingInXuebaka = window.sessionStorage.getItem('usingInXuebaka')
     this.nav_menu_data = JSON.parse(window.sessionStorage.getItem('Auths'))
+    this.xuebakaMenus = this.nav_menu_data.filter(menu=>{
+      if(menu.type == 0){
+        return true
+      }
+      return false
+    })
+    this.deviceMenus = this.nav_menu_data.filter(menu=>{
+      if(menu.type == 3){
+        return true
+      }
+      return false
+    })
+    this.groupControlMenus = this.nav_menu_data.filter(menu=>{
+      if(menu.type == 4){
+        return true
+      }
+      return false
+    })
+    this.bigflowMenus = this.nav_menu_data.filter(menu=>{
+      if(menu.type == 1){
+        return true
+      }
+      return false
+    })
+    
+    // console.log('*****' + JSON.stringify(this.deviceMenus ))
   },
   methods: {
     onRouteChanged () {
@@ -107,13 +233,26 @@ export default {
       that.path = that.$route.path;
     },
     handleOpen (key, keyPath) {
-      // console.log(key, keyPath);
+      console.log('handleOpen')
+      console.log(key, keyPath);
+      // this.openedMenus = []
+      if(key === 'root-xbk'){
+        this.openedMenus = ['root-xbk', key]
+      } else if(key === 'root-device'){
+        this.openedMenus = ['root-device', key]
+      }else if(key === 'root-groupControl'){
+        this.openedMenus = ['root-groupControl', key]
+      }else if(key === 'root-bigflow'){
+        this.openedMenus = ['root-bigflow', key]
+      }
+      
     },
     handleClose (key, keyPath) {
       // console.log(key, keyPath);
     },
     handlselect (index, indexPath) {
-      // console.log(index, indexPath);
+      console.log('handlselect')
+      console.log(index, indexPath);
     },
     // 调用 注册vuex内注册的editableTabs方法
     // ...mapActions({
