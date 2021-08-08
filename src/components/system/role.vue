4e3d156a3a810f8442956913385c1f7e15ee4471
liug1121 <61231796@qq.com>
2021-08-08 15:51:25 +0800

    commit

--- src/components/system/role.vue
+++ src/components/system/role.vue
@@ -26,24 +26,40 @@
           </template>
         </el-table-column>
       </el-table>
-      <!-- 分页区域 -->
-      <!-- <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="page" :page-sizes="[10,20,30]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper"
-        :total="total">
-      </el-pagination> -->
     </el-card>
     <el-dialog title="角色功能" :visible.sync="functionDlgShow" width="630px" >
-      <el-table ref="functionSelTable" v-loading="loading"  :data="roleFunctions" border max-height="600" align="center" :cell-style="{height: '38px',padding:0}" @selection-change="handleRoleFunctionsSelectionChange">
+      <el-table ref="functionSelTable" v-loading="loading"  :data="roleMenus" border max-height="600" align="center" :cell-style="{height: '38px',padding:0}" 
+      @selection-change="handleRoleFunctionsSelectionChange" @select='onRoleFunctionsSelect' @select-all="onRoleFunctionsSelectAll">
         <el-table-column type="selection" width="55">
         </el-table-column>
         <el-table-column v-for="(p, key) in function_column" :prop="p.prop" :label="p.label" :width="p.width" :key="key" align="center" :fixed="p.fixed?p.fixed:false" :sortable="p.sortable">
           <template slot-scope="scope">
               <div v-html="scope.row[p.prop]" />
+              <div v-if="p.prop == 'setting' && scope.row['opts']">
+                <el-button  size="mini" type="warning" plain @click="optSelDlgOpen(scope.row)">设置功能</el-button>
+              </div>
           </template>
         </el-table-column>
       </el-table>
       <span slot="footer" class="dialog-footer">
         <el-button @click="functionSelDlgClose">取 消</el-button>
         <el-button type="primary" @click="editRoleFunctionOk">确 定</el-button>
+      </span>
+    </el-dialog>
+    <el-dialog title="设置操作" :visible.sync="optSelDlgShow" width="630px" >
+      <el-table ref="optSelTable" v-loading="loading"  :data="selectedMenu.opts" border max-height="600" align="center" :cell-style="{height: '38px',padding:0}" 
+      @selection-change="handleOptSelectionChange" @select='onOptsSelect' @select-all="onOptsSelectAll">
+        <el-table-column type="selection" width="55">
+        </el-table-column>
+        <el-table-column v-for="(p, key) in opt_column" :prop="p.prop" :label="p.label" :width="p.width" :key="key" align="center" :fixed="p.fixed?p.fixed:false" :sortable="p.sortable">
+          <template slot-scope="scope">
+              <div v-html="scope.row[p.prop]" />
+          </template>
+        </el-table-column>
+      </el-table>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="optSelDlgClose">取 消</el-button>
+        <el-button type="primary" @click="optSelDlgOk">确 定</el-button>
       </span>
     </el-dialog>
     <el-dialog title="角色" :visible.sync="roleDlgShow" width="430px" >
@@ -72,10 +88,16 @@ export default {
     roles:[],
     selectedRow:{},
     roleFunctions:[],
+    roleMenus:[],
+    roleOpts:[],
+    selectedMenu:{},
     roleFunctions2Edit:[],
     functionDlgShow:false,
     roleDlgShow:false,
+    optSelDlgShow:false,
     roleInfo:{},
+    operating:false,
+    optOperating:false,
       page: 1,
       pageSize: 10,
       // 列表总条数
@@ -86,10 +108,12 @@ export default {
         { prop: 'operations', label: '操作', width: 300}
       ],
       function_column: [
-          
         { prop: 'plateformTypeName', label: '平台类型', width: 200},
-        { prop: 'functionName', label: '功能名称', width: 200},
-        { prop: 'typeName', label: '类型', width: 100}
+        { prop: 'functionName', label: '菜单名', width: 200},
+        { prop: 'setting', label: '功能', width: 100}
+      ],
+      opt_column: [
+        { prop: 'functionName', label: '操作', width: 200}
       ]
     };
     
@@ -103,10 +127,10 @@ export default {
   watch: {},
   updated(){
      this.refreshRoleFunctionsSelected() 
+     this.refreshOptsSelected()
   },
   methods: {
     removeRole:function(role){
-        console.log(JSON.stringify(role))
         let that = this
         let roleId = role.id
         this.$confirm('您确认要此操作, 是否继续?', '提示', {
@@ -168,11 +192,20 @@ export default {
 
     },
     refreshRoleFunctionsSelected:function(){
-        this.roleFunctions.forEach(roleFunction => {
-            if (roleFunction.has == true) {    
-                this.$refs.functionSelTable.toggleRowSelection(roleFunction, true) 
+        this.roleMenus.forEach(roleMenu => {
+            if (roleMenu.has == true) {    
+                this.$refs.functionSelTable.toggleRowSelection(roleMenu, true) 
             }
         })
+    },
+    refreshOptsSelected:function(){
+        if(this.selectedMenu.opts != undefined){
+            this.selectedMenu.opts.forEach(opt => {
+            if (opt.has == true) {    
+                this.$refs.optSelTable.toggleRowSelection(opt, true) 
+            }
+        })
+        }
     },
     functionSel:function(role){
         this.selectedRow = role
@@ -183,8 +216,24 @@ export default {
     functionSelDlgClose:function(){
         this.functionDlgShow = false;
     },
+    optSelDlgOpen:function(row){
+        this.selectedMenu = row
+        this.optSelDlgShow = true
+        this.operating = true
+    },
+    optSelDlgClose:function(){
+        this.optSelDlgShow = false
+    },
+    optSelDlgOk:function(){
+        let opts = this.selectedMenu.opts.filter(opt=> opt.has)
+        if(opts != undefined && opts.length > 0){
+            this.$refs.functionSelTable.toggleRowSelection(this.selectedMenu, true)
+        }else{
+            this.$refs.functionSelTable.toggleRowSelection(this.selectedMenu, false)
+        }
+        this.optSelDlgClose() 
+    },
     handleSelectBranchCom:function(item){
-        console.log('handleSelectBranchCom:' + item)
     },
     addRole:function(roleName, mSuccess, mError){
         let params = {}
@@ -206,11 +255,32 @@ export default {
         apiSystem.getRoleFunctions(params).then(res=>{
             if(res.resultCode == 0){
                 this.roleFunctions = res.data
+                this.roleMenus = this.roleFunctions.filter(one=>{
+                    if(one.type == 0)
+                        return true
+                    return false
+                })
+                
+                this.roleOpts = this.roleFunctions.filter(one=>{
+                    if(one.type == 1)
+                        return true
+                    return false
+                })
+                this.roleMenus.forEach(menu=>{
+                    let opts = []
+                    this.roleOpts.forEach(roleOpt=>{
+                        if(menu.function_id == roleOpt.parentId)
+                            opts.push(roleOpt)
+                    })
+                    if(opts.length > 0)
+                        menu.opts = opts
+                })
                 this.loading = false
             }
         })
     },
     editRoleFunctionOk:function(){
+      this.loading = true
       let that = this
       this.$confirm('您确认要此操作, 是否继续?', '提示', {
         confirmButtonText: '确定',
@@ -218,8 +288,12 @@ export default {
         type: 'warning'
       }).then(() => {
         that.editRoleFunctions(res=>{
+            let roleId = that.selectedRow.id
+            that.getRoleFunctions(roleId)
+            that.loading = false
             alert('修改成功')
         }, res=>{
+            that.loading = false
             alert('修改失败')
         })
         
@@ -230,9 +304,19 @@ export default {
         let roleId = this.selectedRow.id
         let params = {}
         params.roleId = roleId
-        let functionIds = []
-        this.roleFunctions2Edit.forEach(one=>{
-            functionIds.push(one.function_id)
+        let functions2Edit = []
+        functions2Edit = this.roleMenus.filter(roleMenu=> roleMenu.has)
+        functions2Edit.forEach(roleMenu2Edit=>{
+            if(roleMenu2Edit.opts != undefined){
+                let opts2Edit = roleMenu2Edit.opts.filter(opt=> opt.has)
+                opts2Edit.forEach(opt2Edit=>{
+                    functions2Edit.push(opt2Edit)
+                })
+            }
+        })
+        
+        let functionIds = functions2Edit.map(fun2Edit=>{
+            return fun2Edit.function_id
         })
         params.functionIds = functionIds
         apiSystem.editRoleFunctions(params).then(res=>{
@@ -248,13 +332,6 @@ export default {
     queryAllRoles:function(){
         this.loading = true
         let params = {}
-    //     params.page = this.page
-    //     if(this.id != '')
-    //         params.channelId = this.id
-    //     if(this.name != '')
-    //         params.name = this.name
-    //     if(this.appId != '')
-    //         params.appId = this.appId
         apiSystem.getAllSysRoles(params).then(res=>{
             if(res.resultCode == 0){
                 this.roles = res.data
@@ -266,8 +343,41 @@ export default {
     handleSelectionChange:function(val){
 
     },
+    onRoleFunctionsSelectAll(rows){
+        this.onRoleFunctionsSelect(rows, null)
+    },
+    onRoleFunctionsSelect(rows, row){
+        // console.log("rows:" + JSON.stringify(rows))
+        this.roleMenus.forEach(roleMenu=>{
+            roleMenu.has = false
+            rows.forEach(row=>{
+                if(roleMenu.function_id == row.function_id){
+                        roleMenu.has = true;
+                }
+            })
+        }) 
+    },
     handleRoleFunctionsSelectionChange (val) {
-        this.roleFunctions2Edit = val;
+    },
+    onOptsSelect(rows, row){
+        console.log('rows:' + JSON.stringify(rows))
+        let that = this
+        this.selectedMenu.opts.forEach(opt=>{
+            opt.has = false
+            this.selectedMenu.has = false
+            rows.forEach(row=>{
+                if(opt.function_id == row.function_id){
+                    opt.has = true
+                    that.selectedMenu.has = true
+                }
+            })
+        })  
+    },
+    onOptsSelectAll(rows){
+        this.onOptsSelect(rows, null)
+    },
+    handleOptSelectionChange(val){
+        
     },
     handleSizeChange (newPage) {
       this.page = newPage;
